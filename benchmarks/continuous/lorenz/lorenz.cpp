16fde86f443f5642c79367c87aac14e17770bde0
Xin Chen <chenxin415@gmail.com>
2022-08-14 23:13:46 -0400

    New benchmarks added

---
+++ benchmarks/continuous/lorenz/lorenz.cpp
@@ -0,0 +1,99 @@
+#include "../../../flowstar-toolbox/Continuous.h"
+
+using namespace flowstar;
+using namespace std;
+
+
+int main()
+{
+	Variables vars;
+
+	int x_id = vars.declareVar("x");
+	int y_id = vars.declareVar("y");
+	int z_id = vars.declareVar("z");
+
+	ODE<Real> ode({"10*(y - x)", "x*(28 - z) - y", "x*y - 2.67*z"}, vars);
+
+	Computational_Setting setting(vars);
+
+	// using the following statement to specify the stepsize as 0.01 and the order as 7
+	setting.setFixedStepsize(0.01, 8);
+
+	setting.setCutoffThreshold(1e-10);
+
+
+	double w = 0.001; // radius of the initial set
+
+	// define the initial set which is a box
+	Interval init_x(15-w,15+w), init_y(15-w,15+w), init_z(36-w,36+w);
+
+	// ignored dimensions will be associated with the range 0
+	vector<Interval> box(vars.size());
+	box[x_id] = init_x;
+	box[y_id] = init_y;
+	box[z_id] = init_z;
+
+
+	// creating the initial set using the box
+	Flowpipe initialSet(box);
+
+
+	// no unsafe constraints
+	vector<Constraint> unsafeSet;
+
+
+	Result_of_Reachability result;
+
+
+	clock_t begin, end;
+	begin = clock();
+
+
+	Symbolic_Remainder sr(initialSet, 1000);
+
+	double T = 7;
+
+	ode.reach(result, initialSet, T, setting, unsafeSet, sr);
+
+
+	end = clock();
+	printf("time cost: %lf\n", (double)(end - begin) / CLOCKS_PER_SEC);
+
+	if(!result.isCompleted()) // if the flowpipes are successfully computed
+	{
+		printf("Flowpipe computation is terminated due to the large overestimation.\n");
+	}
+
+	if(result.isSafe()) // if all of the flowpipes are safe
+	{
+		printf("All flowpipes are safe.\n");
+	}
+	else if(result.isUnsafe()) // if there is an unsafe flowpipe detected, the computation terminates immediately
+	{
+		printf("The last flowpipe is unsafe.\n");
+	}
+	else // there is no unsafe flowpipe found, but some of the flowpipes intersect the unsafe set
+	{
+		printf("The safety is unknown.\n");
+	}
+
+
+
+	// the following content is only used for plotting
+
+	// flowpipes should be translated to single Taylor model vectors before plotting
+	result.transformToTaylorModels(setting);
+
+
+	Plot_Setting plot_setting(vars);
+	plot_setting.printOn();
+	plot_setting.discreteOutput();	// only plotting the overapproximations at the end of time steps
+	plot_setting.setOutputDims("x", "y");
+	plot_setting.plot_2D_octagon_GNUPLOT("./", "lorenz_x_y", result.tmv_flowpipes);
+
+	plot_setting.setOutputDims("y", "z");
+	plot_setting.plot_2D_octagon_GNUPLOT("./", "lorenz_y_z", result.tmv_flowpipes);
+
+	return 0;
+}
+
---
+++ benchmarks/continuous/robertson/robertson.cpp
@@ -0,0 +1,110 @@
+#include "../../../flowstar-toolbox/Continuous.h"
+
+using namespace flowstar;
+using namespace std;
+
+
+int main()
+{
+	Variables vars;
+
+	int x_id = vars.declareVar("x");
+	int y_id = vars.declareVar("y");
+	int z_id = vars.declareVar("z");
+	int t_id = vars.declareVar("t");
+
+
+	ODE<Real> ode({	"-0.4*x + 1000*y*z",
+					"0.4*x - 1000*y*z - 1e7*y^2",
+					"1e7*y^2",
+					"1"}, vars);
+
+	// set the reachability parameters
+	Computational_Setting setting(vars);
+
+	// set the stepsize and the order
+	setting.setAdaptiveStepsize(0.00001, 0.05, 4);
+
+	// set the cutoff threshold
+	setting.setCutoffThreshold(1e-15);
+
+	// set up the remainder estimation
+	Interval I(-1e-10, 1e-10);
+	vector<Interval> remainder_estimation(vars.size(), I);
+	setting.setRemainderEstimation(remainder_estimation);
+
+	double w = 0;
+
+	// a larger initial set
+//	double w = 1e-3;
+
+	Interval init_x(1-w, 1+w);
+
+	// define the initial set
+	vector<Interval> box(vars.size());
+	box[x_id] = init_x;
+
+
+	Flowpipe initialSet(box);
+
+
+	// unsafe set
+	vector<Constraint> unsafeSet;
+
+	/*
+	 * The structure of the class Result_of_Reachability is defined as below:
+	 * nonlinear_flowpipes: the list of computed flowpipes
+	 * tmv_flowpipes: translation of the flowpipes, they will be used for further analysis
+	 * fp_end_of_time: the flowpipe at the time T
+	 */
+	Result_of_Reachability result;
+
+	// run the reachability computation
+	clock_t begin, end;
+	begin = clock();
+
+	ode.reach(result, initialSet, 40, setting, unsafeSet);
+
+	end = clock();
+	printf("time cost: %lf\n", (double)(end - begin) / CLOCKS_PER_SEC);
+
+	if(!result.isCompleted()) // if the flowpipes are successfully computed
+	{
+		printf("Flowpipe computation is terminated due to the large overestimation.\n");
+	}
+
+	if(result.isSafe()) // if all of the flowpipes are safe
+	{
+		printf("All flowpipes are safe.\n");
+	}
+	else if(result.isUnsafe()) // if there is an unsafe flowpipe detected, the computation terminates immediately
+	{
+		printf("The last flowpipe is unsafe.\n");
+	}
+	else // there is no unsafe flowpipe found, but some of the flowpipes intersect the unsafe set
+	{
+		printf("The safety is unknown.\n");
+	}
+
+
+	// flowpipes should be translated to single Taylor model vectors before plotting
+	result.transformToTaylorModels(setting);
+
+
+	Plot_Setting plot_setting(vars);
+	plot_setting.printOn();
+	plot_setting.setOutputDims("t", "x");
+	plot_setting.plot_2D_interval_GNUPLOT("./", "robertson_t_x", result.tmv_flowpipes);
+
+	plot_setting.setOutputDims("t", "y");
+	plot_setting.plot_2D_interval_GNUPLOT("./", "robertson_t_y", result.tmv_flowpipes);
+
+	plot_setting.setOutputDims("t", "z");
+	plot_setting.plot_2D_interval_GNUPLOT("./", "robertson_t_z", result.tmv_flowpipes);
+
+
+
+
+	return 0;
+}
+
